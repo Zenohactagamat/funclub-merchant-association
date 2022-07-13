e856329d5ab014bfdae1e07448125f293091e37b
Florian Grignoux <florian.grignoux@gmail.com>
2022-07-13 18:24:54 +0200

    feat: added core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 import { AssociationsComponent } from './pages/associations/associations.component';
 import { AppRoutingModule } from './routing/app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, AssociationsComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
