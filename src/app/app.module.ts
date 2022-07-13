f58b3d06d780c20c64d8b03a82bd7df5cb227f44
Florian Grignoux <florian.grignoux@gmail.com>
2022-07-13 18:18:41 +0200

    feat: added shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { AssociationsComponent } from './pages/associations/associations.component';
 import { AppRoutingModule } from './routing/app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, AssociationsComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
