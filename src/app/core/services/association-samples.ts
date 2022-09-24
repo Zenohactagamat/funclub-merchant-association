bf78f93ee4c6228385ebe260886c356fd64ef568
Florian Grignoux <florian.grignoux@gmail.com>
2022-09-24 16:31:34 +0200

    test: added lipsum

--- src/app/core/services/association-samples.ts
+++ src/app/core/services/association-samples.ts
@@ -7,9 +7,16 @@ export const associationSamples = [
     category: 'Danse',
     activity: 'Rock',
     logo: 'https://picsum.photos/200',
-    description: `Une asso qui bouge, sauf en été.
-
-      Profitez d'une semaine gratuite supplémentaire !`,
+    description: `Lorem ipsum dolor sit amet, tempor esse pariatur ut aliqua pariatur dolor ut sit
+ labore incididunt elit ad incididunt enim est aute eiusmod magna. Consequat nis
+i nulla exercitation laborum nostrud commodo eiusmod consectetur quis elit. Sint
+ laborum non laboris esse ex. Proident excepteur fugiat adipisicing laboris ulla
+mco qui ea et deserunt non nostrud pariatur cillum ut magna. Et proident et pari
+atur est exercitation fugiat. Magna quis tempor commodo laborum elit laboris off
+icia do. Nisi minim ex nostrud officia fugiat est labore. In mollit amet cupidat
+at mollit. Ut sint dolor ad aliquip elit sint amet id excepteur. Deserunt dolor
+irure veniam aliqua minim magna pariatur eiusmod culpa officia dolore.
+`,
     availableCoupons: [
       {
         uuid: 'JNBCCDNJNKLJC26sdc1s5dcaas',
@@ -34,9 +41,17 @@ export const associationSamples = [
     category: 'Danse',
     activity: 'Rock',
     logo: 'https://picsum.photos/200',
-    description: `Une asso qui bouge, sauf en été.
+    description: `Lorem ipsum dolor sit amet, magna do nulla esse cupidatat sit minim cillum adipi
+sicing reprehenderit occaecat. Ad ea minim quis ipsum fugiat qui ea dolor. Non c
+upidatat minim mollit aliqua ullamco amet culpa aliqua occaecat enim id est dese
+runt minim nisi. Nostrud ex do do mollit do velit aliqua minim. Fugiat laboris e
 
-      Profitez d'une semaine gratuite supplémentaire !`,
+u proident laboris consequat tempor voluptate qui ea commodo. Ad elit eiusmod si
+t quis esse. Cillum adipisicing dolore eiusmod consectetur qui ex proident irure
+ non consequat. Proident ipsum nostrud non ipsum culpa commodo qui ut. Dolore re
+prehenderit aliquip consectetur. Pariatur eiusmod occaecat labore sit dolor non
+sunt cupidatat culpa id eu deserunt id ipsum nulla.
+`,
     availableCoupons: [
       {
         uuid: 'JNBCCDNJNKLJC26sdc1s5dcaas',
@@ -52,9 +67,20 @@ export const associationSamples = [
     category: 'Danse',
     activity: 'Rock',
     logo: 'https://picsum.photos/200',
-    description: `Une asso qui bouge, sauf en été.
+    description: `Lorem ipsum dolor sit amet, proident aute sunt cillum laboris anim. Dolor veniam
+ quis in consectetur ea deserunt esse cupidatat esse ut laborum id officia anim
+proident proident occaecat. Id duis laborum laboris et reprehenderit deserunt de
+serunt culpa minim exercitation proident ex et aute incididunt deserunt nulla la
+bore. Dolor deserunt in aliqua pariatur aute. Cupidatat cillum elit anim veniam
+sunt consequat ut irure do qui. Amet cupidatat labore elit laborum consequat iru
 
-      Profitez d'une semaine gratuite supplémentaire !`,
+re tempor et ea veniam consequat proident magna. Amet esse esse qui laborum enim
+ laborum ex in sit aliqua qui aliquip commodo Lorem. Nisi proident amet officia
+eiusmod duis eu est sint. Eu adipisicing consequat Lorem qui nostrud proident fu
+
+giat veniam ad laborum ea proident aliqua. Irure tempor voluptate et ipsum ex do
+lor anim elit voluptate eu non anim adipisicing cillum.
+`,
     availableCoupons: [
       {
         uuid: 'JNBCCDNJNKLJC26sdc1s5dcaas',
@@ -70,9 +96,21 @@ export const associationSamples = [
     category: 'Danse',
     activity: 'Rock',
     logo: 'https://picsum.photos/200',
-    description: `Une asso qui bouge, sauf en été.
-
-      Profitez d'une semaine gratuite supplémentaire !`,
+    description: `Lorem ipsum dolor sit amet, laboris ad in reprehenderit excepteur est non duis a
+liquip occaecat ut in ullamco dolor dolore amet. Nulla id non ipsum eiusmod eu e
+xcepteur id aliquip amet proident esse commodo. Adipisicing esse cillum ea conse
+quat nisi deserunt esse dolor quis fugiat. Incididunt commodo est occaecat eu fu
+giat fugiat minim ipsum magna dolor duis fugiat Lorem exercitation duis dolore d
+olor proident ad. Incididunt ut id deserunt laborum excepteur laborum nostrud ex
+cepteur proident voluptate nulla minim consequat adipisicing nulla excepteur par
+iatur commodo. Nisi pariatur culpa dolor. Nisi tempor ad elit dolor do id aliqui
+p elit consequat sunt cupidatat et voluptate occaecat exercitation sunt veniam r
+eprehenderit. Cupidatat elit dolor tempor id non nulla cupidatat est labore pari
+atur occaecat nulla labore ut sint laboris do. Minim voluptate et adipisicing do
+lor tempor commodo occaecat incididunt sunt nostrud cupidatat minim duis laborum
+ ipsum occaecat nostrud nisi esse. Incididunt duis eu laborum laborum veniam Lor
+em occaecat adipisicing qui laboris nulla.
+`,
     availableCoupons: [
       {
         uuid: 'JNBCCDNJNKLJC26sdc1s5dcaas',
@@ -88,9 +126,19 @@ export const associationSamples = [
     category: 'Danse',
     activity: 'Rock',
     logo: 'https://picsum.photos/200',
-    description: `Une asso qui bouge, sauf en été.
-
-      Profitez d'une semaine gratuite supplémentaire !`,
+    description: `Lorem ipsum dolor sit amet, irure magna enim magna eu non duis quis qui minim. M
+agna esse consequat reprehenderit anim anim laborum tempor qui exercitation sint
+ velit eu cupidatat laborum elit cillum voluptate minim nisi. Reprehenderit null
+a proident anim velit ipsum labore nostrud ut pariatur consequat excepteur molli
+t pariatur occaecat et ut. Sunt irure nostrud labore enim excepteur occaecat par
+iatur et consequat consequat ad nulla tempor consectetur qui. Ut magna id conseq
+uat pariatur ullamco Lorem. Cillum dolore amet qui magna ipsum ullamco et ex ex
+officia amet nostrud commodo ut Lorem officia nulla. Dolore sint et cillum duis
+excepteur pariatur do adipisicing minim elit quis et sit occaecat aute ea ut eu.
+ Consectetur sunt proident proident tempor dolore exercitation ut aute. Aliquip
+voluptate minim mollit fugiat nisi proident consequat. Minim do culpa do reprehe
+nderit.
+`,
     availableCoupons: [
       {
         uuid: 'JNBCCDNJNKLJC26sdc1s5dcaas',
@@ -106,9 +154,36 @@ export const associationSamples = [
     category: 'Danse',
     activity: 'Rock',
     logo: 'https://picsum.photos/200',
-    description: `Une asso qui bouge, sauf en été.
-
-      Profitez d'une semaine gratuite supplémentaire !`,
+    description: `Lorem ipsum dolor si
+t amet, dolore anim
+incididunt et nisi e
+iusmod pariatur dese
+runt adipisicing com
+modo qui occaecat id
+ veniam commodo volu
+ptate velit excepteu
+r ex eiusmod mollit
+eu nostrud adipisici
+ng eu amet labore co
+nsequat enim consequ
+at reprehenderit con
+sequat reprehenderit
+ mollit quis enim ut
+ laboris et culpa te
+mpor eiusmod ipsum f
+ugiat in esse minim
+consequat in est off
+icia dolor ea quis a
+liqua anim elit eius
+mod consequat nisi f
+ugiat dolore pariatu
+r irure aute ex labo
+re tempor duis nulla
+ pariatur irure aute
+ labore commodo temp
+or exercitation fugi
+at ex cillum
+`,
     availableCoupons: [
       {
         uuid: 'JNBCCDNJNKLJC26sdc1s5dcaas',
@@ -124,9 +199,17 @@ export const associationSamples = [
     category: 'Danse',
     activity: 'Rock',
     logo: 'https://picsum.photos/200',
-    description: `Une asso qui bouge, sauf en été.
-
-      Profitez d'une semaine gratuite supplémentaire !`,
+    description: `Lorem ipsum dolor sit amet, consequat incididunt est veniam voluptate veniam dolor veniam
+ex laboris est mollit et exercitation cillum labore aliquip elit ullamco nostrud. Excepteu
+r velit ipsum fugiat proident proident. Est ut proident do. Cupidatat exercitation aliqua
+tempor laborum quis sunt do commodo est elit eu sit excepteur amet occaecat. Consequat cup
+idatat anim duis nulla in adipisicing tempor ea adipisicing irure culpa qui dolore cupidat
+at sunt aute laboris dolore. Aliquip exercitation sint reprehenderit exercitation aute sin
+t laborum non veniam deserunt cillum ullamco reprehenderit exercitation sit veniam nisi. U
+llamco non officia ut. Sit veniam amet reprehenderit cillum excepteur dolor dolor reprehen
+derit quis. Mollit ea esse sunt nisi veniam officia laboris eiusmod proident duis. Labore
+irure elit consectetur aute reprehenderit ex incididunt dolore Lorem commodo consequat.
+`,
     availableCoupons: [
       {
         uuid: 'JNBCCDNJNKLJC26sdc1s5dcaas',
@@ -142,9 +225,14 @@ export const associationSamples = [
     category: 'Danse',
     activity: 'Rock',
     logo: 'https://picsum.photos/200',
-    description: `Une asso qui bouge, sauf en été.
-
-      Profitez d'une semaine gratuite supplémentaire !`,
+    description: `Lorem ipsum dolor sit amet, voluptate magna ullamco proident. Est qui adipisicing veniam amet nisi c
+onsequat enim consequat. Aute officia nostrud tempor ex et. Nostrud culpa labore sunt ea irure dolor
+e ipsum et id nisi aute sunt. Tempor mollit exercitation velit aliqua quis occaecat. Dolor pariatur
+ut fugiat eu minim eiusmod est in culpa. Mollit consectetur qui eiusmod reprehenderit. Fugiat conseq
+uat incididunt aute nisi sunt mollit ipsum anim deserunt commodo pariatur ipsum. Occaecat duis incid
+idunt ad tempor quis reprehenderit nulla deserunt laboris officia aliqua officia laborum laboris ull
+amco. Id elit amet ea fugiat non Lorem.
+`,
     availableCoupons: [
       {
         uuid: 'JNBCCDNJNKLJC26sdc1s5dcaas',
